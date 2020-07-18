a923a209c769c80e0a1620c74ad43c13e9e6278e
PRISCILA CAMPOS <priscilacampos@PRISCILAs-MacBook-Pro.local>
2020-07-17 21:46:41 -0500

    changed root path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {checkToken} = require('./auth');
 const {checkRol} = require('./auth');
 const cookieParser = require('cookie-parser');
 const fetch = require('node-fetch');
-//const path = require('../s');
+const path = require('path');
 
 //app.use('/login', express.static(__dirname+"/public/interfaces/sign-in"));
 app.get('/', function(req, res) {

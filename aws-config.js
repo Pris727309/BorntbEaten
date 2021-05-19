61237a7026a8521a47201937bcb095ec0a75b628
Priscila <ms727309@iteso.mx>
2021-05-19 10:45:17 -0500

    removed credentials

--- aws-config.js
+++ aws-config.js
@@ -2,8 +2,6 @@ module.exports = {
     aws_recetas_table_name: 'recetasv2',
     aws_users_table_name: 'usersv4',
     aws_remote_config: {
-      accessKeyId: 'sssss',
-      secretAccessKey: 'sssss',
       region: 'us-east-1',
     }
 };
